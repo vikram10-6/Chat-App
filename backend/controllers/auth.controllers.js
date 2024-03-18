93ded5e10c1d9abdbf4f787cd43e41106734d631
vikram10-6 <vikramayer19@gmail.com>
2024-03-18 23:51:12 +0530

    Initial Commit

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,9 @@
+export const signup = (req, resp) => {
+  console.log("signUp User");
+};
+export const login = (req, resp) => {
+  console.log("Login User");
+};
+export const logout = (req, resp) => {
+  console.log("Logout User");
+};
---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js"
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, resp) => {
+  //root route
+  resp.send("Hello World!!");
+});
+
+app.use("/api/auth",authRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on port ${PORT}`);
+});
