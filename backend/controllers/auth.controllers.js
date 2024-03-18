8412ec7eac96bc8ecd2b23ff46dc496b19f6f553
vikram10-6 <vikramayer19@gmail.com>
2024-03-19 00:22:29 +0530

    Connected  server MongoDb

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,5 +1,10 @@
-export const signup = (req, resp) => {
-  console.log("signUp User");
+export const signup = async(req, resp) => {
+  try {
+    const {fullName,username,password,confirmedPassword,gender}= req.body
+
+  } catch (error) {
+    
+  }
 };
 export const login = (req, resp) => {
   console.log("Login User");
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js"
+import connectToMongoDB from "./DB/connectToMongoDB.js";
 
 const app = express();
 
@@ -15,5 +16,6 @@ app.get("/", (req, resp) => {
 app.use("/api/auth",authRoutes)
 
 app.listen(PORT, () => {
+  connectToMongoDB()
   console.log(`Server is Running on port ${PORT}`);
 });
