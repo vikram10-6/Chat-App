a3ddda1886e1950aa473c6cb8318685d2fd08f27
vikram10-6 <vikramayer19@gmail.com>
2024-03-22 01:51:19 +0530

    added message screen UI

---
+++ frontend/src/components/messages/Messages.jsx
@@ -0,0 +1,18 @@
+import Message from "./Message";
+
+const Messages = () => {
+  return (
+    <div className="px-4 flex-1 overflow-auto scrollbar-hide">
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+    </div>
+  );
+};
+
+export default Messages;
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -2,7 +2,7 @@ import Conversation from "./Conversation";
 
 const Conversations = () => {
   return (
-    <div className="py-2 flex flex-col overflow-auto">
+    <div className="py-2 flex flex-col overflow-auto scrollbar-hide">
       <Conversation />
       <Conversation />
       <Conversation />
