88f3c7fff78ad32baff403490a029232d52080f5
vikram10-6 <vikramayer19@gmail.com>
2024-04-02 01:02:29 +0530

    adddition of socket functionality for real time messaging is completed

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.modal.js";
+import { getReceiverSocketId,io } from "../socket/socket.js";
 
 export const sendMessage = async (req, resp) => {
   try {
@@ -24,8 +25,20 @@ export const sendMessage = async (req, resp) => {
     if (newMessage) {
       conversation.messages.push(newMessage._id);
     }
-    resp.status(200).json(newMessage)
+    // await conversation.save();
+    // await newMessage.save();
+
+    // this will run in parallel
     await Promise.all([conversation.save(), newMessage.save()]);
+
+    // SOCKET IO FUNCTIONALITY WILL GO HERE
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      // io.to(<socket_id>).emit() used to send events to specific client
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
+    resp.status(201).json(newMessage);
   } catch (error) {
     console.log("sM error", error.message);
     resp.status(500).json({ error: "Internal server error" });
@@ -41,8 +54,7 @@ export const getMessages = async (req, resp) => {
       participants: { $all: [senderId, userToChatId] },
     }).populate("messages");
     if (!conversation) return resp.status(200).json([]);
-    resp.status(200).json(conversation.messages)
-    
+    resp.status(200).json(conversation.messages);
   } catch (error) {
     console.log("gM error", error.message);
     resp.status(500).json({ error: "Internal server error" });
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -2,23 +2,26 @@ import { useEffect, useRef } from "react";
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";
+import useListenMessages from "../../hooks/useListenMessages";
 
 const Messages = () => {
   const { messages, loading } = useGetMessages();
-  const lastMessageRef = useRef()
+  useListenMessages();
+  const lastMessageRef = useRef();
 
-  useEffect(()=>{
-    setTimeout(()=>{
-      lastMessageRef.current?.scrollIntoView({behavior:"smooth"})
-    },100)//We use setTimeout because sometime ue to rendering timing this effect alone won't work
-  },[messages])
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 100); //We use setTimeout because sometime  to rendering timing this effect alone won't work
+  }, [messages]);
   return (
     <div className="px-4 flex-1 overflow-auto scrollbar-hide">
       {!loading &&
         messages.length > 0 &&
         messages.map((messages) => (
-          <div className="" key={messages._id}
-          ref = {lastMessageRef} ><Message  message={messages} /></div>
+          <div className="" key={messages._id} ref={lastMessageRef}>
+            <Message message={messages} />
+          </div>
         ))}
       {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
       {!loading && messages.length === 0 && (
