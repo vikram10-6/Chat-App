cd83cc4e4f59e214f1f43d1c74a510316ddcfb41
vikram10-6 <vikramayer19@gmail.com>
2024-04-02 01:43:52 +0530

    did some tweaking for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import userRoutes from "./routes/user.route.js";
 
 import connectToMongoDB from "./DB/connectToMongoDB.js";
-import { app,server } from "./socket/socket.js";
+import { app, server } from "./socket/socket.js";
+
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 
@@ -19,6 +22,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*", (req, resp) => {
+	resp.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
